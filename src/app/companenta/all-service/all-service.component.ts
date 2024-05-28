859b527ec3964faa14abde7b56d041886f22af7a
AbdullohDev07 <gamesupro01@gmail.com>
2024-05-28 18:12:48 +0500

    all-service the end

--- src/app/companenta/all-service/all-service.component.ts
+++ src/app/companenta/all-service/all-service.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-all-service',
   templateUrl: './all-service.component.html',
   styleUrl: './all-service.component.scss'
 })
-export class AllServiceComponent {
+export class AllServiceComponent implements OnInit{
+  news !: AllServices[];
+    constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.getAll()
+  }
 
+    getAll(){
+      this.http.get<AllServices[]>('https://localhost:7048/api/Service/GetAllService?PageIndex=1&Size=11').
+      subscribe((data)=>{
+        this.news = data
+      });
+    }
+}
+
+export interface AllServices {
+  id: string;
+  name: string;
+  servicesId: string;
+  services: any;
 }
