fdcc6779e31197682fe093d6610891aafbf111ed
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-28 16:13:32 +0500

    add news and products

--- src/app/companenta/newest/newest.component.ts
+++ src/app/companenta/newest/newest.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-newest',
   templateUrl: './newest.component.html',
   styleUrl: './newest.component.scss'
 })
-export class NewestComponent {
+export class NewestComponent implements OnInit{
 
+    news !: News[];
+    constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.getAll()
+  }
+
+    getAll(){
+      this.http.get<News[]>('https://localhost:7048/api/News/GetAllNews?PageIndex=1&Size=11').
+      subscribe((data)=>{
+        this.news = data
+      });
+    }
+}
+
+export interface News {
+  id: string;
+  name: string;
+  mainPhotoPath: string;
+  description: string;
 }
--- src/app/companenta/products/products.component.ts
+++ src/app/companenta/products/products.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  products !: CarSeat[]
+  brand !: CarSeatBrand[]
+  size = 3;
+
+  constructor(private http: HttpClient){
+  }
+  ngOnInit(): void {
+    this.getAll()
+  }
+  getAll(){
+    this.http.get<CarSeat[]>(`https://localhost:7048/api/CarSeat/GetAllCarSeat?PageIndex=1&Size=${this.size}`).
+    subscribe((data)=>{
+      this.products = data
+    })
+  }
+
+  getAllBrand(){
+    this.http.get<CarSeatBrand[]>(`https://localhost:7048/api/CarSeatBrand/GetAllCarSeatBrand?PageIndex=1&Size=${this.size}`).
+    subscribe((data)=>{
+      this.brand = data
+    })
+  }
+
+  getAllMass(){
+    this.http.get<CarSeat[]>(`https://localhost:7048/api/CarSeat/GetAllCarSeat?PageIndex=1&Size=${this.size}`).
+    subscribe((data)=>{
+      this.products = data
+    })
+  }
+  seeMore(){
+    this.size=this.size + 8;
+    this.ngOnInit()
+  }
+  seeLess(){
+    this.size=this.size - 8;
+    this.ngOnInit()
+  }
+}
+
+
+export interface CarSeat {
+    id: string;
+    name: string;
+    photoPath: string;
+    price: string;
+    guarantee: string;
+    mass: number;
+    size: string;
+    prodCountry: string;
+    categoryId: string;
+    brandId: string;
+    category: CarSeatCategory;
+    brand: CarSeatBrand;
+}
+export interface CarSeatBrand {
+  id: string;
+  name: string;
+}
+export interface CarSeatCategory {
+  id: string;
+  startAge: string;
+  endAge: string;
 }
