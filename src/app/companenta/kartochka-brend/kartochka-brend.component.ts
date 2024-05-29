d7e6b2dbfdb796d7ebf0a7ee8476af1a285e31fd
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-29 09:50:59 +0500

    add service

--- src/app/companenta/kartochka-brend/kartochka-brend.component.ts
+++ src/app/companenta/kartochka-brend/kartochka-brend.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Service } from '../all-service/all-service.component';
 
 @Component({
   selector: 'app-kartochka-brend',
   templateUrl: './kartochka-brend.component.html',
   styleUrl: './kartochka-brend.component.scss'
 })
-export class KartochkaBrendComponent {
+export class KartochkaBrendComponent implements OnInit {
 
+  services !: Service[];
+  constructor(private http :HttpClient){}
+  ngOnInit(): void {
+    this.getAllServices()
+  }
+
+  getAllServices(){
+    this.http.get<Service[]>('https://localhost:7048/api/Service/GetAllService?PageIndex=1&Size=10').
+    subscribe((data)=>{
+      this.services = data;
+    })
+  }
 }
