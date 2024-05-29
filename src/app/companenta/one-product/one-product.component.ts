e6832dd8521c78557597f86570f89a08ad809e93
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-29 11:59:27 +0500

    add one product

--- src/app/companenta/one-product/one-product.component.ts
+++ src/app/companenta/one-product/one-product.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { CarSeat } from './../products/products.component';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-one-product',
   templateUrl: './one-product.component.html',
   styleUrl: './one-product.component.scss'
 })
-export class OneProductComponent {
+export class OneProductComponent implements OnInit{
+  product !: CarSeat;
+  aydi : string | null = localStorage.getItem("pr_id");
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.getCarSeat(this.aydi ?? "9812d13c-9418-4dcf-87df-9676a7a028c5" );
+  }
 
+  getCarSeat(id:string){
+    this.http.get<CarSeat>(`https://localhost:7048/api/CarSeat/GetByIdCarSeat/${id}`).
+    subscribe((data)=>{
+      this.product = data;
+      console.log(data)
+    });
+  }
 }
